83b7a25a4077307d6d1e63e97b91d72b818601fa
johndgiese <johndgiese@gmail.com>
2014-01-12 14:08:57 -0500

    Start using the templating engine

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -3,9 +3,9 @@
 class IndexController extends \Phalcon\Mvc\Controller {
   public function indexAction() {
     $this->logger->log("Inside the indexAction!");
-    $response = new Phalcon\Http\Response();
-    $response->setStatusCode(200, "OK");
-    $response->setContent("<html><body>Hello Ian</body></html>");
-    $response->send();
+    $context = array(
+      "name" => "Ian",
+    );
+    echo $this->view->render("home", $context);
   }
 }
--- index.php
+++ index.php
@@ -14,12 +14,16 @@ try {
   });
 
   $di->set('view', function() {
-    $view = new \Phalcon\Mvc\View();
+    $view = new \Phalcon\Mvc\View\Simple();
     $view->setViewsDir('views/');
+    $view->registerEngines(array(
+      ".html" => 'Phalcon\Mvc\View\Engine\Volt'
+    ));
     return $view;
   });
 
   $application = new \Phalcon\Mvc\Application($di);
+  $application->useImplicitView(false);
   echo $application->handle()->getContent();
 
 } catch(Exception $e) {
